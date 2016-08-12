72e28d813748982d4d34a5f85cbf4eabbfb3ffb5
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-08-11 17:38:44 -0700

    tests for get on /items

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
-var Storage = function() {
-  this.items = [];
-  this.id = 0;
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+var Storage = {
+  items: [],
+  id: 0,
 };
 
-Storage.prototype.add = function(name) {
+Storage.add = function(name) {
   var item = {name: name, id: this.id};
   this.items.push(item);
   this.id += 1;
   return item;
 };
 
-var storage = new Storage();
+var storage = Object.create(Storage);
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
 
-var app = express();
-app.use(express.static('public'));
-
 app.get('/items', function(req, res) {
   res.status(200).json(storage.items);
 });
+
+
 
 app.listen(8080, function() {
   console.log('Your app is now running on port 8080');
--- test/server.test.js
+++ test/server.test.js
@@ -9,12 +9,29 @@ var storage = server.storage;
 chai.use(chaiHttp);
 
 describe('GET request on /items', function() {
-  it('valid request should return list of items', function(done) {
+  it('valid endpoint should return status 200 and list of items', function(done) {
   	chai.request(app)
   		.get('/items')
   		.end(function(err, res) {
-  			console.log('this happened');
+  			should.equal(err, null);
+  			res.should.have.status(200);
+  			res.should.be.json;
+  			res.body.should.be.a('array');
+  			res.body.length.should.equal(storage.items.length);
   			done();
   		});	
   });
+
+  it('invalid endpoint should return 404 error', function() {
+  	chai.request(app)
+  		.get('/aaaaa')
+  		.end(function(err, res) {
+  			err.should.have.status(404);
+  			done();
+  		});
+  });
+});
+
+describe('POST request on /items', function() {
+
 });
